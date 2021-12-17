a4fc104916f3558e02e5b29a90077c96bdb8ac8a
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-12-16 19:00:40 -0800

    Setup utility functions

--- server/server.js
+++ server/server.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const { removeDuplicates, generateRandomArray } = require('./utility')
 const app = express();
 
 app.get('/', (req, res) => {
-  const poses = JSON.parse(fs.readFileSync('./poses.json'))
-
-  const randomPoses = [];
-  for (let i = 0; i < 5; i++) {
-    const randomNumber = Math.floor(Math.random() * (poses.length ))
-    randomPoses.push(poses[randomNumber])
-  }
-  // Sets don't allow duplicate values
-  const set = new Set()
-  const uniquePoses = randomPoses.filter(el => {
-    // Checks if the set has an id already
-    const duplicate = set.has(el.id);
-    // Adds the id to the set
-    set.add(el.id);
-    // Will only add a value to the array if it doesn't belong to set
-    return !duplicate;
-  });
-  res.json(uniquePoses)
+  const poses = JSON.parse(fs.readFileSync('./poses.json'));
+  const randomPoses = generateRandomArray(poses);
+  const uniquePoses = removeDuplicates(randomPoses);
+  res.status(200).json(uniquePoses);
 })
 
 app.listen(8080, () => {
---
+++ server/utility.js
@@ -0,0 +1,24 @@
+const removeDuplicates = (array) => {
+  // Sets don't allow duplicate values
+  const set = new Set()
+  const uniqueArray = array.filter(el => {
+    // Checks if the set has an id already
+    const duplicate = set.has(el.id);
+    // Adds the id to the set
+    set.add(el.id);
+    // Will only add a value to the array if it doesn't belong to set
+    return !duplicate;
+  });
+  return uniqueArray;
+}
+
+const generateRandomArray = (array) => {
+  const randomArray = [];
+  for (let i = 0; i < 5; i++) {
+    const randomNumber = Math.floor(Math.random() * (array.length));
+    randomArray.push(array[randomNumber]);
+  }
+  return randomArray;
+}
+
+module.exports = { removeDuplicates, generateRandomArray }
