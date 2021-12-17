2be58c6a69369407edce99ee110e958d20e70a4b
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-12-16 18:47:21 -0800

    Setup initial route

--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
-const poses = [
-  {
-    name: 'Cat Cow',
-    description: 'Back stretch .....',
-    bodyPart: 'back',
-    id: '1',
-    image: '',
-    level: 'Beginner'
-  },
-  {
-    name: 'Cobra',
-    description: 'Back stretch .....',
-    bodyPart: 'back',
-    id: '2',
-    image: '',
-    level: 'Beginner'
-  },
-  {
-    name: 'Triangle',
-    description: 'Hamstring stretch .....',
-    bodyPart: 'leg',
-    id: '3',
-    image: '',
-    level: 'Beginner'
-  },
-]
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+  const poses = JSON.parse(fs.readFileSync('./poses.json'))
+
+  const randomPoses = [];
+  for (let i = 0; i < 5; i++) {
+    const randomNumber = Math.floor(Math.random() * (poses.length ))
+    randomPoses.push(poses[randomNumber])
+  }
+  // Sets don't allow duplicate values
+  const set = new Set()
+  const uniquePoses = randomPoses.filter(el => {
+    // Checks if the set has an id already
+    const duplicate = set.has(el.id);
+    // Adds the id to the set
+    set.add(el.id);
+    // Will only add a value to the array if it doesn't belong to set
+    return !duplicate;
+  });
+  res.json(uniquePoses)
+})
+
+app.listen(8080, () => {
+  console.log('Listening on 8080');
+})
