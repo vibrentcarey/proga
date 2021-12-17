826aeaf3beccdeb1a0e72f11bee6bdc484442e94
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-12-16 17:02:46 -0800

    Setup gitignore

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const poses = [
+  {
+    name: 'Cat Cow',
+    description: 'Back stretch .....',
+    bodyPart: 'back',
+    id: '1',
+    image: '',
+    level: 'Beginner'
+  },
+  {
+    name: 'Cobra',
+    description: 'Back stretch .....',
+    bodyPart: 'back',
+    id: '2',
+    image: '',
+    level: 'Beginner'
+  },
+  {
+    name: 'Triangle',
+    description: 'Hamstring stretch .....',
+    bodyPart: 'leg',
+    id: '3',
+    image: '',
+    level: 'Beginner'
+  },
+]
